8ed6f20821114d31b31bc4709cd638ea4aa57603
Adrian Grünberg <adriang2000@gmx.de>
2022-10-03 14:54:14 +0000

    naming adjustments and comments

--- tests/initialize.cpp
+++ tests/initialize.cpp
@@ -3,9 +3,11 @@
 #include "./catch2/catch.hpp"
 #include "./helpers/testHelper.h"
 
-TEST_CASE( "OcppEngine start and stop" ) {
+TEST_CASE( "OcppEngine lifecycle" ) {
+
     //set console output to the cpp console to display outputs
     ao_set_console_out(cpp_console_out);
+
     //initialize OcppEngine with dummy socket
     ArduinoOcpp::OcppEchoSocket echoSocket;
     OCPP_initialize(echoSocket);
