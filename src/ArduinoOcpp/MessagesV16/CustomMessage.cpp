ecd80fd39f99683478390a6525a20eedd6f617c0
matth-x <63792403+matth-x@users.noreply.github.com>
2023-03-28 19:00:05 +0200

    add Custom Message wrapper

---
+++ src/ArduinoOcpp/MessagesV16/CustomMessage.cpp
@@ -0,0 +1,99 @@
+// matth-x/ArduinoOcpp
+// Copyright Matthias Akstaller 2019 - 2023
+// MIT License
+
+#include <ArduinoOcpp/MessagesV16/CustomMessage.h>
+
+using ArduinoOcpp::Ocpp16::CustomMessage;
+
+CustomMessage::CustomMessage(const char *operationType,
+            std::function<std::unique_ptr<DynamicJsonDocument> ()> fn_createReq,
+            std::function<void (JsonObject)> fn_processConf,
+            std::function<bool (const char*, const char*, JsonObject)> fn_processErr,
+            std::function<void (StoredOperationHandler*)> fn_initiate,
+            std::function<bool (StoredOperationHandler*)> fn_restore) :
+        operationType{operationType},
+        fn_createReq{fn_createReq},
+        fn_processConf{fn_processConf},
+        fn_processErr{fn_processErr},
+        fn_initiate{fn_initiate},
+        fn_restore{fn_restore} {
+    
+}
+
+CustomMessage::CustomMessage(const char *operationType,
+            std::function<void (JsonObject)> fn_processReq,
+            std::function<std::unique_ptr<DynamicJsonDocument> ()> fn_createConf,
+            std::function<const char* ()> fn_getErrorCode,
+            std::function<const char* ()> fn_getErrorDescription,
+            std::function<std::unique_ptr<DynamicJsonDocument> ()> fn_getErrorDetails) :
+        operationType{operationType},
+        fn_processReq{fn_processReq},
+        fn_createConf{fn_createConf},
+        fn_getErrorCode{fn_getErrorCode},
+        fn_getErrorDescription{fn_getErrorDescription},
+        fn_getErrorDetails{fn_getErrorDetails} {
+    
+}
+
+CustomMessage::~CustomMessage() {
+
+}
+
+const char* CustomMessage::getOcppOperationType() {
+    return operationType.c_str();
+}
+
+void CustomMessage::initiate(StoredOperationHandler *opStore) {
+    if (fn_initiate) {
+        fn_initiate(opStore);
+    }
+}
+
+bool CustomMessage::restore(StoredOperationHandler *opStore) {
+    if (fn_restore) {
+        return fn_restore(opStore);
+    } else {
+        return false;
+    }
+}
+
+std::unique_ptr<DynamicJsonDocument> CustomMessage::createReq() {
+    return fn_createReq();
+}
+
+void CustomMessage::processConf(JsonObject payload) {
+    return fn_processConf(payload);
+}
+
+void CustomMessage::processReq(JsonObject payload) {
+    return fn_processReq(payload);
+}
+
+std::unique_ptr<DynamicJsonDocument> CustomMessage::createConf() {
+    return fn_createConf();
+}
+
+const char *CustomMessage::getErrorCode() {
+    if (fn_getErrorCode) {
+        return fn_getErrorCode();
+    } else {
+        return nullptr;
+    }
+}
+
+const char *CustomMessage::getErrorDescription() {
+    if (fn_getErrorDescription) {
+        return fn_getErrorDescription();
+    } else {
+        return "";
+    }
+}
+
+std::unique_ptr<DynamicJsonDocument> CustomMessage::getErrorDetails() {
+    if (fn_getErrorDetails) {
+        return fn_getErrorDetails();
+    } else {
+        return createEmptyDocument();
+    }
+}
---
+++ src/ArduinoOcpp/MessagesV16/CustomMessage.h
@@ -0,0 +1,69 @@
+// matth-x/ArduinoOcpp
+// Copyright Matthias Akstaller 2019 - 2023
+// MIT License
+
+#ifndef FUNCTIONALMESSAGE_H
+#define FUNCTIONALMESSAGE_H
+
+#include <ArduinoOcpp/Core/OcppMessage.h>
+
+#include <functional>
+
+namespace ArduinoOcpp {
+
+namespace Ocpp16 {
+
+class CustomMessage : public OcppMessage {
+private:
+    std::string operationType;
+    std::function<void (StoredOperationHandler*)> fn_initiate; //optional
+    std::function<bool (StoredOperationHandler*)> fn_restore;  //optional
+    std::function<std::unique_ptr<DynamicJsonDocument> ()> fn_createReq;
+    std::function<void (JsonObject)> fn_processConf;
+    std::function<bool (const char*, const char*, JsonObject)> fn_processErr;  //optional
+    std::function<void (JsonObject)> fn_processReq;
+    std::function<std::unique_ptr<DynamicJsonDocument> ()> fn_createConf;
+    std::function<const char* ()> fn_getErrorCode;            //optional
+    std::function<const char* ()> fn_getErrorDescription;     //optional
+    std::function<std::unique_ptr<DynamicJsonDocument> ()> fn_getErrorDetails; //optional
+public:
+
+    //for operations initiated at this device
+    CustomMessage(const char *operationType,
+            std::function<std::unique_ptr<DynamicJsonDocument> ()> fn_createReq,
+            std::function<void (JsonObject)> fn_processConf,
+            std::function<bool (const char*, const char*, JsonObject)> fn_processErr = nullptr,
+            std::function<void (StoredOperationHandler*)> fn_initiate = nullptr,
+            std::function<bool (StoredOperationHandler*)> fn_restore = nullptr);
+    
+    //for operations receied from remote
+    CustomMessage(const char *operationType,
+            std::function<void (JsonObject)> fn_processReq,
+            std::function<std::unique_ptr<DynamicJsonDocument> ()> fn_createConf,
+            std::function<const char* ()> fn_getErrorCode = nullptr,
+            std::function<const char* ()> fn_getErrorDescription = nullptr,
+            std::function<std::unique_ptr<DynamicJsonDocument> ()> fn_getErrorDetails = nullptr);
+    
+    ~CustomMessage();
+
+    const char* getOcppOperationType() override;
+
+    void initiate(StoredOperationHandler *opStore) override;
+
+    bool restore(StoredOperationHandler *opStore) override;
+
+    std::unique_ptr<DynamicJsonDocument> createReq() override;
+
+    void processConf(JsonObject payload) override;
+
+    void processReq(JsonObject payload) override;
+
+    std::unique_ptr<DynamicJsonDocument> createConf() override;
+    const char *getErrorCode() override;
+    const char *getErrorDescription() override;
+    std::unique_ptr<DynamicJsonDocument> getErrorDetails() override;
+};
+
+} //end namespace Ocpp16
+} //end namespace ArduinoOcpp
+#endif
