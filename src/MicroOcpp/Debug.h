7185fb515b326f8de21725f4380ef038b024742b
matth-x <63792403+matth-x@users.noreply.github.com>
2023-09-13 15:48:35 +0200

    add compact debug msg option

--- src/MicroOcpp/Debug.h
+++ src/MicroOcpp/Debug.h
@@ -19,9 +19,9 @@
 #endif
 
 #define MOCPP_DF_MINIMAL 0x00   //don't reveal origin of a debug message
-#define MOCPP_DF_MODULE_LINE 0x01 //print module by file name and line number
+#define MOCPP_DF_COMPACT 0x01   //print module by file name and line number
 #define MOCPP_DF_FILE_LINE 0x02 //print file and line number
-#define MOCPP_DF_FULL    0x03   //print path and file and line numbr
+#define MOCPP_DF_FULL 0x03      //print path and file and line numbr
 
 #ifndef MOCPP_DBG_FORMAT
 #define MOCPP_DBG_FORMAT MOCPP_DF_FILE_LINE //default
@@ -31,12 +31,11 @@
 #if MOCPP_DBG_FORMAT == MOCPP_DF_MINIMAL
 #define MOCPP_DBG(level, X)   \
     do {                        \
-        MOCPP_CONSOLE_PRINTF("[OCPP] ");           \
         MOCPP_CONSOLE_PRINTF X;  \
         MOCPP_CONSOLE_PRINTF("\n");         \
     } while (0);
 
-#elif MOCPP_DBG_FORMAT == MOCPP_DF_MODULE_LINE
+#elif MOCPP_DBG_FORMAT == MOCPP_DF_COMPACT
 #define MOCPP_DBG(level, X)   \
     do {                        \
         const char *_mo_file = __FILE__;         \
@@ -46,7 +45,7 @@
             _mo_l--;         \
             if (_mo_file[_mo_l] == '.') _mo_r = _mo_l;         \
         }         \
-        MOCPP_CONSOLE_PRINTF("[OCPP] %s (%.*s:%i): ",level, (int) (_mo_r - _mo_l), _mo_file + _mo_l,__LINE__);           \
+        MOCPP_CONSOLE_PRINTF("%.*s:%i ", (int) (_mo_r - _mo_l), _mo_file + _mo_l,__LINE__);           \
         MOCPP_CONSOLE_PRINTF X;  \
         MOCPP_CONSOLE_PRINTF("\n");         \
     } while (0);
