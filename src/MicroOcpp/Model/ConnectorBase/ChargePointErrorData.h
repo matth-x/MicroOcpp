fb6a765b1ac8240e060f2e65ef0f3022aaafa73f
Matthias Akstaller <63792403+matth-x@users.noreply.github.com>
2024-07-23 18:21:28 +0200

    StatusNotification error layering mechanism (#331)
    * add option to report NoError
    
    * add build switch MO_REPORT_NOERROR
    
    * add error severity levels
    
    * double-check NoError condition
    
    * revise stacked error conditions
    
    * update changelog

--- src/MicroOcpp/Model/ConnectorBase/ChargePointErrorData.h
+++ src/MicroOcpp/Model/ConnectorBase/ChargePointErrorData.h
@@ -5,11 +5,14 @@
 #ifndef MO_CHARGEPOINTERRORCODE_H
 #define MO_CHARGEPOINTERRORCODE_H
 
+#include <stdint.h>
+
 namespace MicroOcpp {
 
 struct ErrorData {
     bool isError = false; //if any error information is set
     bool isFaulted = false; //if this is a severe error and the EVSE should go into the faulted state
+    uint8_t severity = 1; //severity: don't send less severe errors during highly severe error condition
     const char *errorCode = nullptr; //see ChargePointErrorCode (p. 76/77) for possible values
     const char *info = nullptr; //Additional free format information related to the error
     const char *vendorId = nullptr; //vendor-specific implementation identifier
