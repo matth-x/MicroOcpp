a9213ec1b9af220a1a08744c5f36e3f68f79495a
Matthias Akstaller <63792403+matth-x@users.noreply.github.com>
2024-08-15 10:53:39 +0200

    Correct MV PreBoot year 1970 timestamps (#354)
    * fix MeterValues PreBoot year 1970 timestamps
    
    * update changelog

--- src/MicroOcpp/Operations/MeterValues.cpp
+++ src/MicroOcpp/Operations/MeterValues.cpp
@@ -14,12 +14,12 @@ using MicroOcpp::JsonDoc;
 #define ENERGY_METER_TIMEOUT_MS 30 * 1000  //after waiting for 30s, send MeterValues without missing readings
 
 //can only be used for echo server debugging
-MeterValues::MeterValues() : MemoryManaged("v16.Operation.", "MeterValues") {
+MeterValues::MeterValues(Model& model) : MemoryManaged("v16.Operation.", "MeterValues"), model(model) {
     
 }
 
-MeterValues::MeterValues(Vector<std::unique_ptr<MeterValue>>&& meterValue, unsigned int connectorId, std::shared_ptr<Transaction> transaction) 
-      : MemoryManaged("v16.Operation.", "MeterValues"), meterValue{std::move(meterValue)}, connectorId{connectorId}, transaction{transaction} {
+MeterValues::MeterValues(Model& model, Vector<std::unique_ptr<MeterValue>>&& meterValue, unsigned int connectorId, std::shared_ptr<Transaction> transaction) 
+      : MemoryManaged("v16.Operation.", "MeterValues"), model(model), meterValue{std::move(meterValue)}, connectorId{connectorId}, transaction{transaction} {
     
 }
 
@@ -36,8 +36,15 @@ std::unique_ptr<JsonDoc> MeterValues::createReq() {
     size_t capacity = 0;
     
     auto entries = makeVector<std::unique_ptr<JsonDoc>>(getMemoryTag());
-    for (auto value = meterValue.begin(); value != meterValue.end(); value++) {
-        auto entry = (*value)->toJson();
+    for (auto mv = meterValue.begin(); mv != meterValue.end(); mv++) {
+
+        if ((*mv)->getTimestamp() < MIN_TIME) {
+            MO_DBG_DEBUG("adjust preboot MeterValue timestamp");
+            Timestamp adjusted = model.getClock().adjustPrebootTimestamp((*mv)->getTimestamp());
+            (*mv)->setTimestamp(adjusted);
+        }
+
+        auto entry = (*mv)->toJson();
         if (entry) {
             capacity += entry->capacity();
             entries.push_back(std::move(entry));
