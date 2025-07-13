1c80d131dd5d3be36fd4d3bc889eb4663992e603
matth-x <63792403+matth-x@users.noreply.github.com>
2025-07-13 18:03:46 +0200

    fix workflows

--- examples/ESP/main.cpp
+++ examples/ESP/main.cpp
@@ -24,7 +24,7 @@ ESP8266WiFiMulti WiFiMulti;
 //
 //  Settings which worked for my SteVe instance:
 //
-//#define OCPP_BACKEND_URL   "ws://192.168.178.100:8180/steve/websocket/CentralSystemService"
+//#define OCPP_BACKEND_URL   "ws://192.168.178.100:8080/steve/websocket/CentralSystemService"
 //#define OCPP_CHARGE_BOX_ID "esp-charger"
 
 void setup() {
@@ -58,22 +58,32 @@ void setup() {
     /*
      * Initialize the OCPP library
      */
-    mocpp_initialize(OCPP_BACKEND_URL, OCPP_CHARGE_BOX_ID, "My Charging Station", "My company name");
+    mo_initialize();
+
+    mo_setWebsocketUrl(
+            OCPP_BACKEND_URL,   //OCPP backend URL
+            OCPP_CHARGE_BOX_ID, //ChargeBoxId, assigned by OCPP backend
+            nullptr,            //Authorization key (`nullptr` disables Basic Auth)
+            nullptr);           //OCPP backend TLS certificate (`nullptr` disables certificate check)
+
+    mo_setBootNotificationData("My Charging Station", "My company name");
+
+    mo_setOcppVersion(MO_OCPP_V16); //Run OCPP 1.6. For v2.0.1, pass argument `MO_OCPP_V201`
 
     /*
      * Integrate OCPP functionality. You can leave out the following part if your EVSE doesn't need it.
      */
-    setEnergyMeterInput([]() {
+    mo_setEnergyMeterInput([]() {
         //take the energy register of the main electricity meter and return the value in watt-hours
-        return 0.f;
+        return 0;
     });
 
-    setSmartChargingCurrentOutput([](float limit) {
+    mo_setSmartChargingCurrentOutput([](float limit) {
         //set the SAE J1772 Control Pilot value here
         Serial.printf("[main] Smart Charging allows maximum charge rate: %.0f\n", limit);
     });
 
-    setConnectorPluggedInput([]() {
+    mo_setConnectorPluggedInput([]() {
         //return true if an EV is plugged to this EVSE
         return false;
     });
@@ -86,12 +96,12 @@ void loop() {
     /*
      * Do all OCPP stuff (process WebSocket input, send recorded meter values to Central System, etc.)
      */
-    mocpp_loop();
+    mo_loop();
 
     /*
      * Energize EV plug if OCPP transaction is up and running
      */
-    if (ocppPermitsCharge()) {
+    if (mo_ocppPermitsCharge()) {
         //OCPP set up and transaction running. Energize the EV plug here
     } else {
         //No transaction running at the moment. De-energize EV plug
@@ -103,7 +113,7 @@ void loop() {
     if (/* RFID chip detected? */ false) {
         String idTag = "0123456789ABCD"; //e.g. idTag = RFID.readIdTag();
 
-        if (!getTransaction()) {
+        if (!mo_getTransactionIdTag()) {
             //no transaction running or preparing. Begin a new transaction
             Serial.printf("[main] Begin Transaction with idTag %s\n", idTag.c_str());
 
@@ -112,7 +122,7 @@ void loop() {
              * and listen to the ConnectorPlugged Input. When the Authorization succeeds and an EV
              * is plugged, the OCPP lib will send the StartTransaction
              */
-            auto ret = beginTransaction(idTag.c_str());
+            auto ret = mo_beginTransaction(idTag.c_str());
 
             if (ret) {
                 Serial.println(F("[main] Transaction initiated. OCPP lib will send a StartTransaction when" \
@@ -122,15 +132,10 @@ void loop() {
             }
 
         } else {
-            //Transaction already initiated. Check if to stop current Tx by RFID card
-            if (idTag.equals(getTransactionIdTag())) {
-                //card matches -> user can stop Tx
-                Serial.println(F("[main] End transaction by RFID card"));
-
-                endTransaction(idTag.c_str());
-            } else {
-                Serial.println(F("[main] Cannot end transaction by RFID card (different card?)"));
-            }
+            //Transaction already initiated. Attempt to stop current Tx by RFID card. If idTag is the same,
+            //or in the same group, then MO will stop the transaction and `mo_ocppPermitsCharge()` becomes
+            //false
+            mo_endTransaction(idTag.c_str(), nullptr); //`idTag` and `reason` can be nullptr
         }
     }
 
